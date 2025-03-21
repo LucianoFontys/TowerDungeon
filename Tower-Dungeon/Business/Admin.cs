82e71f4eb64b65643d74cc1819fe3c976c32e0f4
Luciano Llera <417189@student.fontys.nl>
2025-03-21 11:24:53 +0100

    Added People classes

---
+++ Tower-Dungeon/Business/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tower_Dungeon.Business
+{
+    class Admin : Person
+    {
+        private string username;
+        private bool isAdmin = true;
+        public Admin(int id, string fName, string lName, string email, string password, string username) : base(id, fName, lName, email, password)
+        {
+            this.username = username;
+        }
+    }
+}
---
+++ Tower-Dungeon/Business/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tower_Dungeon.Business
+{
+    class Person
+    {
+        private int id;
+        private string fName;
+        private string lName;
+        private string email;
+        private string password;
+
+        public Person(int id, string fName, string lName, string email, string password)
+        {
+            this.id = id;
+            this.fName = fName;
+            this.lName = lName;
+            this.email = email;
+            this.password = password;
+        }
+
+
+    }
+}
